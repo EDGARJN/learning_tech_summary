fd9d6bf39566db0e1c635b1c10b4ff1261c240ab
EDGAR JN <asetceo01@gmail.com>
2023-08-11 17:51:49 +0300

    angular ordering app for learning purpose

---
+++ angularHero/ordering_app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes:Recipe[] = [new Recipe("Sambusa","It includes sausages","https://shorturl.at/yBDE6")];
+
+}
