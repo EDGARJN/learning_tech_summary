554cf3193fb0e49abedb1773b0b405e7cda75d13
EDGAR JN <asetceo01@gmail.com>
2023-08-14 10:54:59 +0300

    add recipe list function

--- angularHero/ordering_app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angularHero/ordering_app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,4 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent {
   recipes:Recipe[] = [new Recipe("Sambusa","It includes sausages","https://shorturl.at/yBDE6"), new Recipe("Vegeterian Recipe","specific for meat hater","https://shorturl.at/belBR")];
 
+  addRecipe(){
+    this.recipes.push( new Recipe("Vegeterian Recipe","specific for meat hater","https://shorturl.at/belBR"))
+  }
+
 }
