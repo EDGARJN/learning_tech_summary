1918348bca2aa63af8b5dff35533eaa8077f99a6
EDGAR JN <asetceo01@gmail.com>
2023-08-21 20:15:07 +0300

    add routes and enable SPA navigation

--- angularHero/ordering_app/src/app/app-routing.module.ts
+++ angularHero/ordering_app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:AppComponent},
+  {path:"recipes",component:RecipesComponent, children:[
+    {path:":id",component:RecipeDetailsComponent}
+  ]},
+  {path:"shopping", component:ShopingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
